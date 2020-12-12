8c2191358de35f24cacf3e0410d88e2a945d04bd
tuminzee <t.sheth33@gmail.com>
2020-12-12 22:37:42 +0530

    Removed ignore files

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'mongoUrl' : "mongodb+srv://root:root@cluster0.wi5ns.gcp.mongodb.net/JWT?retryWrites=true&w=majority",
-    'token_secret': 'ilovemeanstack',
-    'email': 'sehatintel@gmail.com',
-    'password': 'sehat@MEFGI'
-}
-
-
